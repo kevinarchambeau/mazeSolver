e59b7d262c76c94c0557179c05734b8a962a2901
kevinarchambeau <xxerexx@gmail.com>
2024-06-17 20:11:33 -0700

    implemented Cell class and draw method

--- src/draw.py
+++ src/draw.py
@@ -41,3 +41,38 @@ class Point:
     def __init__(self, x, y):
         self.x = x
         self.y = y
+
+
+class Cell:
+    def __init__(self, x1, y1, x2, y2, win, left_wall=True, right_wall=True, top_wall=True, bottom_wall=True):
+        self.x1 = x1
+        self.y1 = y1
+        self.x2 = x2
+        self.y2 = y2
+        self.win = win
+        self.left_wall = left_wall
+        self.right_wall = right_wall
+        self.top_wall = top_wall
+        self.bottom_wall = bottom_wall
+
+    def draw(self):
+        tl_x = self.x1
+        tl_y = self.y1
+        br_x = self.x2
+        br_y = self.y2
+        if self.left_wall:
+            # from upper left to lower left
+            left_line = Line(Point(tl_x, tl_y), Point(tl_x, br_y))
+            self.win.draw_line(left_line, "black")
+        if self.top_wall:
+            # upper left to upper right
+            top_line = Line(Point(tl_x, tl_y), Point(br_x, tl_y))
+            self.win.draw_line(top_line, "red")
+        if self.right_wall:
+            # upper right to lower right
+            right_line = Line(Point(br_x, tl_y), Point(br_x, br_y))
+            self.win.draw_line(right_line, "purple")
+        if self.bottom_wall:
+            # bottom right to lower right
+            bottom_line = Line(Point(tl_x, br_y), Point(br_x, br_y))
+            self.win.draw_line(bottom_line, "blue")
--- src/main.py
+++ src/main.py
@@ -1,6 +1,10 @@
-from draw import Window, Line, Point
+from draw import Window, Line, Point, Cell
 
 win = Window(800, 600)
 line = Line(Point(50,50), Point(400,400))
-win.draw_line(line, "black")
+win.draw_line(line, "blue")
+cell = Cell(10, 10, 50, 50, win, True, True, True, True)
+cell.draw()
+cell = Cell(55, 10, 95, 50, win, True, True, True, True)
+cell.draw()
 win.wait_for_close()
